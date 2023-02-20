23b15ce32c674e6894b86fe924712bc35a23b080
Lebogang Mosenthal <lebogang.mosenthal@live.com>
2023-02-20 16:17:16 +0200

    prettier format

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Register from "./Register"
+import Register from "./Register";
 
-const  App=()=> {
+const App = () => {
   return (
     <main className="App">
-      <Register/>
+      <Register />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
